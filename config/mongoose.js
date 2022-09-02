909213a9b498935a74d3d2d59e214b648f3a50cf
Akshay Vinayak <akshaykaran2@gmail.com>
2022-09-02 19:51:36 +0530

    mongo uri setup

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongo_uri = "mongodb+srv://akshayvinayak:Narak%4009051997@cluster0.s9v8bsy.mongodb.net/Data"
+
+const mongo_uri = process.env.MONGO_URI;
   // "mongodb+srv://anshurai:anshurai1998@cluster0.trmqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(mongo_uri);
